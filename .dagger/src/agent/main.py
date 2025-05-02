cf40c7156148da740eb96061084e99f6eb04cefe
Vikram Vaswani <vikram@dagger.io>
2025-05-02 16:38:56 +0000

    Updated agent
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- .dagger/src/agent/main.py
+++ .dagger/src/agent/main.py
@@ -51,7 +51,7 @@ class Agent:
             dag.env()
             .with_workspace_input("before", dag.workspace(source=source), "the workspace to use for code and tests")
             .with_workspace_output("after", "the workspace with the modified code")
-            .with_string_output("summary", "explanation of the changes made")
+            .with_string_output("summary", "proposed list of changes for the tests to pass")
         )
 
         prompt = f"""
@@ -67,8 +67,7 @@ class Agent:
         - Do not assume that errors are related to database connectivity or initialization
         - Focus only on Python files within the /app directory
         - Do not interact directly with the database; use the test tool only
-        - Once done, return the modified workspace and a summary of the changes made
-        - The summary should be written as a proposal explaining the changes to be made
+        - Once done, return the modified workspace and a proposed list of changes
         """
         work = (
             dag.llm()
@@ -110,7 +109,7 @@ class Agent:
         #)
 
         #summary = await work.last_reply()
-        comment = f"diff: {diff}\n\nsummary: {summary}"
+        comment = f"{summary}\n\n```{diff}```"
         return await dag.workspace(source=source, token=token).comment(repository, ref, comment)
 
         #print(f"diff: {diff}\n\nsummary: {summary}")
